7dfe2f85135ac3514ee60f7e925b2488b109dac6
David Carbol <dcarbol@vendavo.com>
2015-01-07 17:25:15 +0100

    Added log4j (+logback), POM, Idea project...

---
+++ src/main/java/com/davidicius/dnc/Starter.java
@@ -0,0 +1,12 @@
+package com.davidicius.dnc;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Starter {
+    private static final Logger log = LoggerFactory.getLogger(Starter.class);
+
+    public static void main(String[] args) {
+       log.debug("DNC 0.1 Started...");
+    }
+}
