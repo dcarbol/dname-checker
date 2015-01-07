13177ddfaf0fd42056d164340c5d90a3b5f5b0dd
David Carbol <dcarbol@vendavo.com>
2015-01-07 17:39:44 +0100

    HTTPComponents client

--- src/main/java/com/davidicius/dnc/Starter.java
+++ src/main/java/com/davidicius/dnc/Starter.java
@@ -1,12 +1,51 @@
 package com.davidicius.dnc;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
 
 public class Starter {
     private static final Logger log = LoggerFactory.getLogger(Starter.class);
 
-    public static void main(String[] args) {
-       log.debug("DNC 0.1 Started...");
+    public static void main(String[] args) throws IOException {
+        log.debug("DNC 0.1 Started...");
+
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        try {
+            HttpGet httpget = new HttpGet("http://www.nic.cz/whois/");
+
+            System.out.println("Executing request " + httpget.getRequestLine());
+
+            // Create a custom response handler
+            ResponseHandler<String> responseHandler = new ResponseHandler<String>() {
+
+                public String handleResponse(
+                        final HttpResponse response) throws ClientProtocolException, IOException {
+                    int status = response.getStatusLine().getStatusCode();
+                    if (status >= 200 && status < 300) {
+                        HttpEntity entity = response.getEntity();
+                        return entity != null ? EntityUtils.toString(entity) : null;
+                    } else {
+                        throw new ClientProtocolException("Unexpected response status: " + status);
+                    }
+                }
+
+            };
+            String responseBody = httpclient.execute(httpget, responseHandler);
+            System.out.println("----------------------------------------");
+            System.out.println(responseBody);
+        } finally {
+            httpclient.close();
+        }
     }
 }
+
